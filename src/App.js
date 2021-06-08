13c0d0fc4c26847e4a9ad0d689b1b240a9cf2c54
Abhijeet Mishra <abhijeet.mishra@zemosolabs.com>
2021-06-08 16:16:22 +0530

    sometry

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-app">
+      <div className="flex-app-items header header-left item-1"><h2>Pizza Restaurant</h2> </div>
+      <div className="flex-app-items header header-right item-2"><h1>Hey Honey! How did we come here!</h1></div>
+      <div className="flex-app-items item-3"><h3>Description</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, velit! Perferendis nobis impedit eveniet obcaecati similique itaque voluptate distinctio vitae?</p></div>
+      <div className="flex-app-items item-4"><div><img src='https://images.pexels.com/photos/708587/pexels-photo-708587.jpeg'></img></div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, velit! Perferendis nobis impedit eveniet obcaecati similique itaque voluptate distinctio vitae?</p></div>
+      <div className="flex-app-items item-5"><h3>Description</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, velit! Perferendis nobis impedit eveniet obcaecati similique itaque voluptate distinctio vitae?</p></div>
+      <div className="flex-app-items item-6"><div><img src='https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'></img></div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, velit! Perferendis nobis impedit eveniet obcaecati similique itaque voluptate distinctio vitae?</p></div>
+
+      </div>
     </div>
   );
 }
